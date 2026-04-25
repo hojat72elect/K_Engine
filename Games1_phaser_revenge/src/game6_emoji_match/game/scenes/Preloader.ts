2b4bd46cf67195547fb76e7798ae9a02e3f42134
hojat72elect <hojat72elect@gmail.com>
2026-04-25 19:08:37 -0400

    Added game 6 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game6_emoji_match/game/scenes/Preloader.ts
@@ -0,0 +1,41 @@
+export class Preloader extends Phaser.Scene {
+
+    loadText!: Phaser.GameObjects.Text;
+
+    constructor() {
+        super('Preloader');
+    }
+
+    preload() {
+        this.loadText = this.add.text(400, 360, 'Loading ...', {fontFamily: 'Arial', fontSize: 64, color: '#e3f2ed'});
+
+        this.loadText.setOrigin(0.5);
+        this.loadText.setStroke('#203c5b', 6);
+        this.loadText.setShadow(2, 2, '#2d2d2d', 4, true, false);
+
+        this.load.setPath('assets/game6_emoji_match');
+        this.load.image(['background', 'logo'] as any as Phaser.Types.Loader.FileTypes.ImageFileConfig[]);
+        this.load.multiatlas('emojis', 'emojis.json');
+
+        //  Audio ...
+        this.load.setPath('assets/game6_emoji_match/sounds/');
+
+        this.load.audio('music', ['music.ogg', 'music.m4a', 'music.mp3']);
+        this.load.audio('countdown', ['countdown.ogg', 'countdown.m4a', 'countdown.mp3']);
+        this.load.audio('match', ['match.ogg', 'match.m4a', 'match.mp3']);
+    }
+
+    create() {
+        if (this.sound.locked) {
+            this.loadText.setText('Click to Start');
+
+            this.input.once('pointerdown', () => {
+
+                this.scene.start('MainMenu');
+
+            });
+        } else {
+            this.scene.start('MainMenu');
+        }
+    }
+}
