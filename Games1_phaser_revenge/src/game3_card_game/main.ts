395e5fb45bb21bffdfd19b85f36c122da84eefa6
hojat72elect <hojat72elect@gmail.com>
2026-04-24 23:14:37 -0400

    Added game 3 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game3_card_game/main.ts
@@ -0,0 +1,23 @@
+import {Preloader} from './Preloader.ts';
+import {Play} from './Play.ts';
+import {AUTO, Game, Scale} from 'phaser';
+
+const config = {
+    title: 'Card Memory Game',
+    type: AUTO,
+    width: 549,
+    height: 480,
+    parent: 'game-container',
+    backgroundColor: '#192a56',
+    pixelArt: true,
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        Preloader,
+        Play
+    ]
+};
+
+new Game(config);
