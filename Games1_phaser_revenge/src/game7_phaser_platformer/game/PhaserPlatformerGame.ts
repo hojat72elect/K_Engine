fd29b902bec118b0687dd635222be5cb426bff0c
hojat72elect <hojat72elect@gmail.com>
2026-04-25 19:27:14 -0400

    Added game 7 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game7_phaser_platformer/game/PhaserPlatformerGame.ts
@@ -0,0 +1,165 @@
+import {Math, Scene} from "phaser";
+import Text = Phaser.GameObjects.Text;
+import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+import Group = Phaser.Physics.Arcade.Group;
+import SpriteWithDynamicBody = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+import GameObject = Phaser.GameObjects.GameObject;
+import ArcadePhysicsCallback = Phaser.Types.Physics.Arcade.ArcadePhysicsCallback;
+
+
+export class PhaserPlatformerGame extends Scene {
+    scoreText!: Text;
+    gameOver = false;
+    score = 0;
+    cursors!: CursorKeys;
+    platforms: StaticGroup;
+    bombs!: Group;
+    stars!: Group;
+    player!: SpriteWithDynamicBody;
+
+    preload() {
+        this.load.image('sky', 'assets/game7_phaser_platformer/sky.png');
+        this.load.image('ground', 'assets/game7_phaser_platformer/platform.png');
+        this.load.image('star', 'assets/game7_phaser_platformer/star.png');
+        this.load.image('bomb', 'assets/game7_phaser_platformer/bomb.png');
+        this.load.spritesheet('dude', 'assets/game7_phaser_platformer/dude.png', {frameWidth: 32, frameHeight: 48});
+    }
+
+    create() {
+        //  A simple background for our game
+        this.add.image(400, 300, 'sky');
+
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        this.platforms = this.physics.add.staticGroup();
+
+        //  Here we create the ground.
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        //  Now let's create some ledges
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+
+        // The player and its settings
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        //  Player physics properties. Give the little guy a slight bounce.
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        //  Our player animations, turning, walking left and walking right.
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{key: 'dude', frame: 4}],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        //  Input Events
+        this.cursors = this.input.keyboard!.createCursorKeys();
+
+        //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: {x: 12, y: 0, stepX: 70}
+        });
+
+        this.stars.children.iterate((child: GameObject) => {
+
+            //  Give each star a slightly different bounce
+            (child as SpriteWithDynamicBody).setBounceY(Math.FloatBetween(0.4, 0.8));
+            return true;
+
+        });
+
+        this.bombs = this.physics.add.group();
+
+        //  The score
+        this.scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', color: '#000'});
+
+        //  Collide the player and the stars with the platforms
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.collider(this.bombs, this.platforms);
+
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        this.physics.add.overlap(this.player, this.stars, this.collectStar as ArcadePhysicsCallback, undefined, this);
+
+        this.physics.add.collider(this.player, this.bombs, this.hitBomb as ArcadePhysicsCallback, undefined, this);
+    }
+
+    update() {
+        if (this.gameOver) {
+            return;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        } else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+    }
+
+    collectStar(player: SpriteWithDynamicBody, star: SpriteWithDynamicBody) {
+        star.disableBody(true, true);
+
+        //  Add and update the score
+        this.score += 10;
+        this.scoreText.setText(`Score: ${this.score}`);
+
+        if (this.stars.countActive(true) === 0) {
+            //  A new batch of stars to collect
+            this.stars.children.iterate((child: GameObject) => {
+                const spriteChild = child as SpriteWithDynamicBody;
+                spriteChild.enableBody(true, spriteChild.x, 0, true, true);
+                return true;
+            });
+
+            const x = (player.x < 400) ? Math.Between(400, 800) : Math.Between(0, 400);
+
+            const bomb = this.bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Math.Between(-200, 200), 20);
+
+        }
+    }
+
+    hitBomb(player: SpriteWithDynamicBody, _bomb: SpriteWithDynamicBody) {
+        this.physics.pause();
+
+        player.setTint(0xff0000);
+
+        player.anims.play('turn');
+
+        this.gameOver = true;
+    }
+}
---
+++ Games1_phaser_revenge/src/game7_phaser_platformer/game/main.ts
@@ -0,0 +1,29 @@
+import {AUTO, Game, Scale, Types} from 'phaser';
+import {PhaserPlatformerGame} from "./PhaserPlatformerGame.ts";
+
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {x: 0, y: 300},
+            debug: false
+        }
+    },
+    parent: 'game-container',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        PhaserPlatformerGame
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({...config, parent});
+}
+
+export default StartGame;
