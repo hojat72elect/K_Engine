35352b7d555f713e48681752b5d0370a01347856
hojat72elect <hojat72elect@gmail.com>
2026-04-25 19:45:20 -0400

    Added game 8 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game8_flood_fill/game/main.ts
@@ -0,0 +1,30 @@
+import {WEBGL, Game, Scale, Types} from 'phaser';
+import {FloodFillGame} from "./FloodFillGame.ts";
+
+const config: Types.Core.GameConfig = {
+    type: WEBGL,
+    width: 800,
+    height: 600,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {x: 0, y: 300},
+            debug: false
+        }
+    },
+    parent: 'game-container',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        FloodFillGame
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({...config, parent});
+}
+
+export default StartGame;
