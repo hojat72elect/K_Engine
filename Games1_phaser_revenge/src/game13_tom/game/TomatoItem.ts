b69af53f77718746b2f7d5b1840a7a2b86aaabdc
hojat72elect <hojat72elect@gmail.com>
2026-04-26 11:20:43 -0400

    Added game 13 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game13_tom/game/TomatoItem.ts
@@ -0,0 +1,24 @@
+import {Math} from 'phaser';
+import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+
+export class TomatoItem extends StaticGroup {
+
+    constructor(config: any) {
+        super(config.physicsWorld, config.scene);
+        this.addTomatoItem();
+    }
+
+    addTomatoItem() {
+        this.create(
+            Math.Between(50, this.scene.scale.width - 50),
+            Math.Between(150, this.scene.scale.height - 70),
+            'tomato_item'
+        );
+    }
+
+    destroyItem() {
+        this.children.entries[0].destroy();
+        this.addTomatoItem();
+    }
+
+}
