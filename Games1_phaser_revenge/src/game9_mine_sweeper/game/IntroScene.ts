77efa00c0770e09296b8e1669e92acd25604a42c
hojat72elect <hojat72elect@gmail.com>
2026-04-25 20:22:22 -0400

    Added game 9 as a separate directory in the same project.

---
+++ Games1_phaser_revenge/src/game9_mine_sweeper/game/IntroScene.ts
@@ -0,0 +1,75 @@
+import Rectangle = Phaser.GameObjects.Rectangle;
+import Image = Phaser.GameObjects.Image;
+
+export class IntroScene extends Phaser.Scene {
+
+    highlight: Rectangle;
+    intro: Image;
+
+    constructor() {
+        super();
+    }
+
+    preload() {
+        this.load.setBaseURL('');
+        this.load.setPath('assets/game9_mine_sweeper');
+
+        this.load.spritesheet('tiles', 'tiles.png', {frameWidth: 16});
+        this.load.spritesheet('digits', 'digits.png', {frameWidth: 13, frameHeight: 23, endFrame: 9});
+        this.load.spritesheet('buttons', 'digits.png', {frameWidth: 26, frameHeight: 26, startFrame: 5});
+        this.load.image('topLeft', 'top-left.png');
+        this.load.image('topRight', 'top-right.png');
+        this.load.image('topBg', 'top-bg.png');
+        this.load.image('botLeft', 'bot-left.png');
+        this.load.image('botRight', 'bot-right.png');
+        this.load.image('botBg', 'bot-bg.png');
+        this.load.image('left', 'left.png');
+        this.load.image('right', 'right.png');
+        this.load.image('intro', 'intro.png');
+        this.load.image('win95', 'win95.png');
+    }
+
+    create() {
+        this.input.mouse!.disableContextMenu();
+
+        this.highlight = this.add.rectangle(0, 334, 800, 70, 0x0182fb).setOrigin(0).setAlpha(0.75);
+
+        this.intro = this.add.image(0, 0, 'intro').setOrigin(0);
+
+        const zone1 = this.add.zone(0, 334, 800, 70).setOrigin(0);
+        const zone2 = this.add.zone(0, 411, 800, 70).setOrigin(0);
+        const zone3 = this.add.zone(0, 488, 800, 70).setOrigin(0);
+
+        zone1.setInteractive();
+        zone2.setInteractive();
+        zone3.setInteractive();
+
+        zone1.on('pointerover', () => {
+            this.highlight.y = zone1.y;
+        });
+
+        zone2.on('pointerover', () => {
+            this.highlight.y = zone2.y;
+        });
+
+        zone3.on('pointerover', () => {
+            this.highlight.y = zone3.y;
+        });
+
+        zone1.once('pointerdown', () => {
+            this.startGame(9, 9, 10);
+        });
+
+        zone2.once('pointerdown', () => {
+            this.startGame(16, 16, 40);
+        });
+
+        zone3.once('pointerdown', () => {
+            this.startGame(16, 30, 99);
+        });
+    }
+
+    startGame(width: number, height: number, bombs: number) {
+        this.scene.start('MineSweeper', {width, height, bombs});
+    }
+}
